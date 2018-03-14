d65ea5d5f2a9eef0cd7e1256258bc0f1a199c7df
lliei0x <git@liei.cc>
2018-03-14 23:56:04 +0800

    modify

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 
 	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding"
 	"golang.org/x/text/transform"
 )
 
@@ -24,14 +26,24 @@ func main() {
 		return
 	}
 
+	e := determineEncoding(reps.Body)
 	utf8Reader := transform.NewReader(reps.Body,
-		simplifiedchinese.GBK.NewDecoder())
-
-	charset.DetermineEncoding()
+		e.NewDecoder())
 
 	all, err := ioutil.ReadAll(utf8Reader)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s\n", all)
+}
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+
 }
