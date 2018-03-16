e4b66f6c30be8d02c1e1340075bccabce5bfa6ff
lliei0x <git@liei.cc>
2018-03-17 00:32:08 +0800

    add fetcher

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -1,47 +1,45 @@
-package main
+package fetcher
 
 import (
 	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 )
 
-func main() {
-
-	reps, err := http.Get("http://www.zhenai.com/zhenghun")
+func Fetch(url string) ([]byte, error) {
+	reps, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	defer reps.Body.Close()
 
 	if reps.StatusCode != http.StatusOK {
-		fmt.Println("Error: Status Code: ", reps.StatusCode)
-		return
+		// fmt.Println("Error: Status Code: ", reps.StatusCode)
+		return nil, fmt.Errorf("wrong status Code: %d", reps.StatusCode)
 	}
 
 	e := determineEncoding(reps.Body)
 	utf8Reader := transform.NewReader(reps.Body,
 		e.NewDecoder())
 
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s\n", all)
+	return ioutil.ReadAll(utf8Reader)
 }
 
 func determineEncoding(r io.Reader) encoding.Encoding {
 
 	bytes, err := bufio.NewReader(r).Peek(1024)
 	if err != nil {
-		panic(err)
+		log.Printf("Fetcher error: %v", err)
+		return unicode.UTF8
 	}
 	e, _, _ := charset.DetermineEncoding(bytes, "")
 	return e
